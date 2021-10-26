894565113c31c1457e9a9d17ee48c88dd9cc5330
Razorr222 <65959891+Razorr222@users.noreply.github.com>
2021-10-26 10:20:09 +0800

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,4 @@
+const {DiscordJS, MessageEmbed} = require('discord.js')
+const SlashCommandBuilder = require('@discordjs/builders')
+const db = require('quick.db')
+const config = require('../config.js')

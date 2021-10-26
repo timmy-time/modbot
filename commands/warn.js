5a00346bcb142a17c4562b51384498ab813acf56
Razorr222 <65959891+Razorr222@users.noreply.github.com>
2021-10-26 19:09:03 +0800

    Summary

--- commands/warn.js
+++ commands/warn.js
@@ -6,20 +6,25 @@ const config = require("../config.js")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("warn")
+        .setDescription('Check or manage Warnings')
         // Adding a SubcommandGroup as 'manage'
         .addSubcommandGroup((group) => 
             group
                 .setName("manage")
+                .setDescription('Manage the warnings of a user.')
                 .addSubcommand((subcommand) => 
                     subcommand
+                        .setName('user')
+                        .setDescription('Decide the user')
                         .addUserOption((option) => 
                             option
-                            .setName('user')
-                        .setDescription('The user whose xp to alter')
-                        .setRequired(false),
+                                .setName('user')
+                                .setDescription('The user whose xp to alter')
+                                .setRequired(false),
                         )
-                        .addIntergerOption((option) => 
-                            option.setName('id')
+                        .addIntegerOption((option) => 
+                            option
+                                .setName('id')
                                 .setDescription("The id of the user to warn")
                                 .setRequired(false)
                         )
@@ -29,6 +34,7 @@ module.exports = {
                 .addSubcommand((subcommand) => 
                     subcommand
                         .setName("add")
+                        .setDescription('Add a warning to the specified user.')
                         .addUserOption((subcommand) => 
                             subcommand
                                 .setName('add')
@@ -42,34 +48,38 @@ module.exports = {
                                 .setDescription('Reason for the warning')
                         )
 
-
-                // Adding a SubcommandGroup as 'remove'
-                .addSubcommandGroup((group) => 
-                    group
+                )
+                // Adding a Subcommand as 'remove'
+                .addSubcommand((subcommand) => 
+                    subcommand
                         .setName("remove")
+                        .setDescription('Remove one of the warnings')
                         .addUserOption((option) =>
                             option
                                 .setName('remove')
                                 .setDescription('Remove a warning from the specificied user.')
                                 .setRequired(true))
-                )
-            )
-
+                        )
+                )   
             // Adding a SubcommandGroup as ''
-            .addSubcommandGroup((group) =>
-                group
-                    .setName("show")
+            .addSubcommand((subcommand) =>
+                subcommand
+                    .setName('show')
+                    .setDescription('Show the users warnings')
                     .addUserOption((option) =>
-                        option
-                            .setName('user')
-                            .setDescription('The user whose xp to alter')
-                            .setRequired(false)
-                    )
-            )                
-    ),
-    async execute(interaction, message, messageCreate, args, option, client) {
+                            option
+                                .setName('user')
+                                .setDescription("The user that you want to see.")
+                                .setRequired(false)
+                    ) 
+            ),  
+    async execute(interaction, message, args, option, client) {
         try {
-            interaction.reply(interaction.guild)
+            interaction.reply(interaction.options.getSubcommandGroup())
+            interaction.reply(interaction.options.getSubcommand())
+            interaction.reply(interaction.options.getString())
+            interaction.reply(interaction.options.getUser())
+            interaction.reply(interaction.options.getInteger())
         } catch (err) {
             console.log(err)
         }
